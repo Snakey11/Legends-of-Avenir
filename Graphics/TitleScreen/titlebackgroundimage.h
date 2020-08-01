97eaec0b30729ea8624694a8ccf1573e7097a803
Snakey11 <snakey1loa@gmail.com>
2020-07-31 20:53:10 -0400

    Title screen work

---
+++ Graphics/TitleScreen/titlebackgroundimage.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(titlebackgroundimage)
+
+//======================================================================
+//
+//	titlebackgroundimage, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ 462 tiles (t|f|p reduced) lz77 compressed
+//	+ regular map (flat), not compressed, 30x20 
+//	Total size: 512 + 13944 + 1200 = 15656
+//
+//	Time-stamp: 2020-07-31, 17:56:45
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TITLEBACKGROUNDIMAGE_H
+#define GRIT_TITLEBACKGROUNDIMAGE_H
+
+#define titlebackgroundimageTilesLen 13944
+extern const unsigned short titlebackgroundimageTiles[6972];
+
+#define titlebackgroundimageMapLen 1200
+extern const unsigned short titlebackgroundimageMap[600];
+
+#define titlebackgroundimagePalLen 512
+extern const unsigned short titlebackgroundimagePal[256];
+
+#endif // GRIT_TITLEBACKGROUNDIMAGE_H
+
+//}}BLOCK(titlebackgroundimage)
