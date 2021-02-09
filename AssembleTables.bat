03af7bba19f67aab01e00f4824a4ae1db9bc11f9
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-02-09 16:52:58 -0500

    Add legacy TableManager.py call

--- AssembleTables.bat
+++ AssembleTables.bat
@@ -4,6 +4,7 @@
 cd %~dp0/CSV/Tables
 
 echo: | (TableManager.exe TableOptions.s TableInstaller.event --error_on_missing)
+rem py TableManager.py TableOptions.s TableInstaller.event --error_on_missing -v
 
 if /I not [%1]==[noPause] (
 	pause
