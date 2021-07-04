70666d489a6141343424478476456c170c69d21d
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-07-04 14:32:34 -0400

    Update AssembleDefs.bat

--- AssembleDefs.bat
+++ AssembleDefs.bat
@@ -3,9 +3,13 @@
 
 cd "%~dp0/Definitions"
 
-py Enumerate.py DefinitionsAssembler.s Definitions.event --clean
+echo: | (MakeSkillDefs)
 
-py MakeParseDefs.py Definitions.event ParseDefinitions.txt ../Text/ParseDefinitions.txt Mug
+rem py Enumerate.py DefinitionsAssembler.s Definitions.event --clean
+echo: | (Enumerate DefinitionsAssembler.s Definitions.event --clean)
+
+rem py MakeParseDefs.py Definitions.event ParseDefinitions.txt ../Text/ParseDefinitions.txt Mug
+echo: | (MakeParseDefs Definitions.event ParseDefinitions.txt ../Text/ParseDefinitions.txt Mug)
 
 if /I not [%1]==[noPause] (
 	pause
