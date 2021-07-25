352ea7adc1e3c78e69da345653a653b39c0699e8
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-07-25 15:27:49 -0400

    Reorg unit menu debug + submenu!

---
+++ ASM/Debug/DebugUnitMenu/DebugUnitMenu.s
@@ -0,0 +1,26 @@
+
+.thumb
+
+.macro blh to, reg
+    ldr \reg, =\to
+    mov lr, \reg
+    .short 0xF800
+.endm
+
+.equ ProcFind, 0x08002E9C
+.equ StartMenuChild, 0x804EBC8
+.equ gProc_MapEventEngine, 0x08591AC0
+
+.global DebugSubmenuEffect
+.type DebugSubmenuEffect, %function
+DebugSubmenuEffect: @ MenuProc* menu, MenuCommandProc* command. Return a status bitfield.
+push { lr }
+ldr r0, =gProc_MapEventEngine
+blh ProcFind, r1
+mov r1, r0
+ldr r0, =gDebugUnitMenuDefinition
+blh StartMenuChild, r2
+mov r0, #0x0B @ ME_END|ME_PLAY_BEEP|ME_CLEAR_GFX = 0x1|0x2|0x8.
+pop { r1 }
+bx r1
+.ltorg
