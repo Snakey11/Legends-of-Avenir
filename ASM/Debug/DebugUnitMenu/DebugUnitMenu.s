ec4d2813242e43300c5b695b9f0ce737cb718185
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-07 16:22:19 -0500

    Re-add enemy control debug

--- ASM/Debug/DebugUnitMenu/DebugUnitMenu.s
+++ ASM/Debug/DebugUnitMenu/DebugUnitMenu.s
@@ -10,6 +10,7 @@
 .equ ProcFind, 0x08002E9C
 .equ StartMenuChild, 0x804EBC8
 .equ gProc_MapEventEngine, 0x08591AC0
+.equ StartMapEventEngine, 0x800D0B0
 
 .global DebugSubmenuEffect
 .type DebugSubmenuEffect, %function
@@ -21,6 +22,18 @@ mov r1, r0
 ldr r0, =gDebugUnitMenuDefinition
 blh StartMenuChild, r2
 mov r0, #0x0B @ ME_END|ME_PLAY_BEEP|ME_CLEAR_GFX = 0x1|0x2|0x8.
+pop { r1 }
+bx r1
+.ltorg
+
+.global EnemyControlMenuEffect
+.type EnemyControlMenuEffect, %function
+EnemyControlMenuEffect: @ MenuProc* menu, MenuCommandProc* command. Return a status bitfield. Called by the debug submenu.
+push { lr }
+ldr r0, =SecretEvent
+mov r1, #0x01
+blh StartMapEventEngine, r2
+mov r0, #0x0B
 pop { r1 }
 bx r1
 .ltorg
