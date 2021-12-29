d332b2a2aa4f100af9ea4c3eba3ee5d8e89e950c
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-29 11:40:16 -0500

    Cache item icon sheet binaries and update AssembleGraphics.bat with new filepaths

--- AssembleGraphics.bat
+++ AssembleGraphics.bat
@@ -5,21 +5,28 @@ setlocal enabledelayedexpansion
 set currDir=%~dp0
 
 
-cd "!currDir!Graphics/Battle Palettes"
+cd "!curDir!Graphics/ItemIcons"
+
+echo Assembling icons...
+
+call "AssembleIcons.bat" noPause
+
+
+cd "!currDir!Graphics/BattlePalettes"
 
 echo Assembling battle palettes...
 
 call "AssemblePalettes.bat" noPause
 
 
-cd "!currDir!Graphics/Class Cards"
+cd "!currDir!Graphics/ClassCards"
 
 echo Assembling class cards...
 
 call "AssembleCards.bat" noPause
 
 
-cd "!currDir!Graphics/Map Sprites"
+cd "!currDir!Graphics/MapSprites"
 
 echo Assembling map sprites...
 
@@ -36,6 +43,7 @@ call "AssemblePortraits.bat" noPause
 cd "!currDir!Graphics/WorldMap"
 
 call "AssembleSmallWM.bat" noPause
+
 
 if /I not [%1]==[noPause] (
 	pause
---
+++ Graphics/ItemIcons/AssembleIcons.bat
@@ -0,0 +1,33 @@
+
+@echo off
+
+setlocal enabledelayedexpansion
+
+rem png2dmp.exe: Calls png2dmp.exe for all .png files in folder & subfolders
+rem does not call png2dmp for files where the existing .dmp file is newer than the .png file
+
+set FILE_MATCH=*.png
+set png2dmp=%~dp0Png2Dmp.exe
+
+for /R "%~dp0" %%F in (%FILE_MATCH%) do (
+    SET SHOULD_COMPILE=0
+    set DUMP_FILE=%%~dF%%~pF%%~nF%.dmp
+    if exist !DUMP_FILE! (
+	For /F "Delims=" %%I In ('dir /b /OD "!DUMP_FILE!" "%%F" ^| more +1') Do Set NEWER=%%I
+	if !NEWER! == %%~nxF (
+	    set SHOULD_COMPILE=1
+	)
+    ) else (
+	set SHOULD_COMPILE=1
+    )
+    if /I "!SHOULD_COMPILE!" EQU "1" (
+	echo Assembling "%%~nxF"...
+	cd "%%~dpF"
+	png2dmp "%%~nxF"
+    )
+)
+
+echo Done!
+if /I not [%1]==[noPause] (
+	pause
+)
