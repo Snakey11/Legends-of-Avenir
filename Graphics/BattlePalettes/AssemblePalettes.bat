2e08b016e76414931740156adec360e7acad62fb
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-31 16:29:38 -0500

    Add if not exist cache to assemblepalettes

--- Graphics/BattlePalettes/AssemblePalettes.bat
+++ Graphics/BattlePalettes/AssemblePalettes.bat
@@ -9,6 +9,8 @@ rem does not call png2dmp for files where the existing .dmp file is newer than t
 set FILE_MATCH=*.png
 set png2dmp=%~dp0Png2Dmp.exe
 
+if not exist cache ( mkdir cache )
+
 for /R "%~dp0" %%F in (%FILE_MATCH%) do (
     set SHOULD_COMPILE=0
 	set DUMP_FILE=%%~dF%%~pFcache\%%~nF%.dmp
