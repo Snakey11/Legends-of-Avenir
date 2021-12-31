24c1a8755a072b93d517cdbad62691b4ce02cdbc
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-31 18:29:47 -0500

    Use cache directory for portraits and update installer

--- Graphics/Portraits/AssemblePortraits.bat
+++ Graphics/Portraits/AssemblePortraits.bat
@@ -9,21 +9,23 @@ rem does not call portraitformatter for files where the existing .dmp file is ne
 set FILE_MATCH=*.png
 set portraitformatter=%~dp0PortraitFormatter.exe
 
+if not exist cache ( mkdir cache )
+
 for /R "%~dp0" %%F in (%FILE_MATCH%) do (
     SET SHOULD_COMPILE=0
-    set DUMP_FILE="%%~dF%%~pF%%~nF%_mug.dmp"
+    set DUMP_FILE="%%~dF%%~pFcache\%%~nF%.dmp"
     if exist !DUMP_FILE! (
-	For /F "Delims=" %%I In ('dir /b /OD "!DUMP_FILE!" %%F ^| more +1') Do Set NEWER=%%I
-	if !NEWER! == %%~nxF (
-	    set SHOULD_COMPILE=1
-	)
+		For /F "Delims=" %%I In ('dir /b /OD "!DUMP_FILE!" %%F ^| more +1') Do Set NEWER=%%I
+		if !NEWER! == %%~nxF (
+			set SHOULD_COMPILE=1
+		)
     ) else (
-	set SHOULD_COMPILE=1
+		set SHOULD_COMPILE=1
     )
     if /I "!SHOULD_COMPILE!" EQU "1" (
 	echo Assembling "%%~nxF"...
 	cd "%%~dpF"
-	portraitformatter "%%~nxF"
+	portraitformatter "%%~nxF" -o "!DUMP_FILE!"
     )
 )
 
