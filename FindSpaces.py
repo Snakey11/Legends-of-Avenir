36445dd36944d0d8413f351dc8b96df5a29ef3d7
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-31 14:28:25 -0500

    Update find spaces script

--- FindSpaces.py
+++ FindSpaces.py
@@ -5,8 +5,15 @@ from os import path
 
 # Quick python script to find spaces in filepaths in the entire repository.
 
+out = 'Spaces.txt'
+
+spaces = [] # List of files with spaces.
+
 if __name__ == '__main__':
     for (dirpath, dirnames, filenames) in walk( os.getcwd() ):
         for f in filenames:
             fullpath = dirpath[len(os.getcwd())+1:]+'\\'+f
-            if ' ' in fullpath: print(f'Space found in {fullpath}')
+            if ' ' in fullpath: spaces.append(f'Space found in {fullpath}\n')
+
+with open(out,'w') as f:
+    f.writelines(spaces)
