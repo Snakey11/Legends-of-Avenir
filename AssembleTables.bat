73bbb99b1781561d1defb4a2909e436ffbe654a1
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-30 22:21:31 -0500

    oops why is AssembleTables running the working TableManager python script instead of the exe on master?

--- AssembleTables.bat
+++ AssembleTables.bat
@@ -3,8 +3,8 @@
 
 cd %~dp0/CSV/Tables
 
-rem echo: | (TableManager.exe TableOptions.s TableInstaller.event --error_on_missing)
-py TableManager.py TableOptions.s TableInstaller.event --error_on_missing
+echo: | (TableManager.exe TableOptions.s TableInstaller.event --error_on_missing)
+rem py TableManager.py TableOptions.s TableInstaller.event --error_on_missing
 
 if /I not [%1]==[noPause] (
 	pause
