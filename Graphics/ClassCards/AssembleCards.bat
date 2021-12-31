9b437fcd9dafc353957818f37d637257b7d1c6d3
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2021-12-31 16:30:02 -0500

    Add cache directory for class cards

--- Graphics/ClassCards/AssembleCards.bat
+++ Graphics/ClassCards/AssembleCards.bat
@@ -9,22 +9,24 @@ rem does not call png2dmp for files where the existing .dmp file is newer than t
 set FILE_MATCH=*.png
 set png2dmp=%~dp0Png2Dmp.exe
 
+if not exist cache ( mkdir cache )
+
 for /R "%~dp0" %%F in (%FILE_MATCH%) do (
     SET SHOULD_COMPILE=0
-    set DUMP_FILE=%%~dF%%~pF%%~nF%.dmp
+    set DUMP_FILE=%%~dF%%~pFcache\%%~nF%.dmp
     if exist !DUMP_FILE! (
-	For /F "Delims=" %%I In ('dir /b /OD "!DUMP_FILE!" "%%F" ^| more +1') Do Set NEWER=%%I
-	if !NEWER! == %%~nxF (
-	    set SHOULD_COMPILE=1
-	)
+		For /F "Delims=" %%I In ('dir /s /b /OD "!DUMP_FILE!" "%%F" ^| more +1') Do Set NEWER=%%I
+		if !NEWER! == %%~nxF (
+			set SHOULD_COMPILE=1
+		)
     ) else (
-	set SHOULD_COMPILE=1
+		set SHOULD_COMPILE=1
     )
     if /I "!SHOULD_COMPILE!" EQU "1" (
 	echo Assembling "%%~nxF"...
 	cd "%%~dpF"
-	png2dmp "%%~nxF" --lz77
-	png2dmp "%%~nxF" --palette-only > "%%~nF%Palette.dmp"
+	png2dmp "%%~nxF" --lz77 -o "!DUMP_FILE!"
+	png2dmp "%%~nxF" --palette-only > "%%~dF%%~pFcache\%%~nF%Palette.dmp"
     )
 )
 
