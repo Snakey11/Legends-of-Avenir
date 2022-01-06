5167dab2c2e9d04398e7f68b0e4778dcb1682104
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2022-01-06 18:30:50 -0500

    Format Mage 3 text

--- Text/Chapters/Mage/Mage3Leave.s
+++ Text/Chapters/Mage/Mage3Leave.s
@@ -1,120 +1,133 @@
 
 ## Mage3LeaveStartingText
-Tact: Teacher, how far is it to Bacrun?
-Xena: I'm not sure on foot. I've always ridden there.
-Ajax: Shame we couldn't have stolen horses before leaving. Not that we had a whole lot of time.
-Tact: Do you think they'll be following us?
-Zaheva: Most likely. Standard procedure for them is to suit up a fast-moving unit, but while they pull soldiers together for it, we've got time.
-Ajax: With the war going on, it could take them even longer with all of this considered.
-Tact: ... Are you okay, Teacher?
-Xena: What? Oh... I'm fine.
-Ajax: You can't believe Mistlain betrayed you out of his fear of Uzkalis, can you?
-Xena: ... No. I can't. I've served him faithfully for years...
-Zaheva: They're all like this.
-Ajax: Zaheva, that's not helpful.
-Zaheva: Unhelpful, but not incorrect.
-Ajax: Just... Keep an eye out behind us, alright? We'll need to know if we're being pursued.
-Zaheva: Alright. I'll hang back.
-Tact: What's her problem?
-Ajax: She's only shared a little with me. We both have issues with the way Arcanae has been headed in recent years, but hers... Her reasons stem from hatred. Something happened to her in her youth. I've never pressed it.
-Xena: I swear I've heard of her before...
-Ajax: I couldn't tell you why.
-Tact: How long do we intend to walk for today?
-Ajax: We'll take a rest once the sun is down. Hopefully we can find somewhere off the main road where we can camp out of sight. For now, let's keep moving.
-[X]
+[ConversationText]
+[MidLeft][LoadXena][MidRight][LoadPlayer]
+Teacher, how far is it to Bacrun?
+[MidLeft]I'm not sure on foot. I've only ridden there.[CloseSpeech][MoveLeft][MidRight][MoveRight][FarRight][LoadZaheva][FarLeft][LoadAjax]
+Shame we couldn't have stolen horses before leaving. Not that we had a whole lot of time.
+[Right]Do you think they'll be following us?
+[FarRight]Most likely. Standard procedure for them is to suit up a fast-moving unit, but while they pull soldiers together for it, we've got time.
+[FarLeft]With the war going on, it could take them even longer with all of this considered.
+[Right]... Are you okay, Teacher?
+[Left][MoveLeft]What? Oh... I'm fine.
+[FarLeft]You can't believe Mistlain betrayed you out of his fear of Uzkalis, can you?
+[Left]... No. I can't. I've served him faithfully for years...
+[FarRight]They're all like this.
+[FarLeft]Zaheva, that's not helpful.
+[FarRight]Unhelpful, but not incorrect.
+[FarLeft]Just... Keep an eye out behind us, alright? We'll need to know if we're being pursued.
+[FarRight]Alright.[.....] I'll hang back.[ClearFace][Right][MoveFarRight][Left][MoveRight][ClearFace][LoadXena][FarRight]
+What's her problem?[CloseSpeech][FarLeft][MoveMidLeft]
+She's only shared a little with me. We both have issues with the way Arcanae has been headed in recent years, but hers... Her reasons stem from hatred. Something happened to her in her youth. I've never pressed it.
+[Right]I swear I've heard of her before...
+[MidLeft]I couldn't tell you why.
+[FarRight]How long do we intend to walk for today?
+[MidLeft]We'll take a rest once the sun is down. Hopefully we can find somewhere off the main road where we can camp out of sight. For now, let's keep moving.[X]
 
 ## Mage3LeaveGettingDarkText
-Xena: It's starting to get dark out. Ajax, should we set up camp?
-Ajax: I see somewhere off road up here that should leave us relatively out of sight. Let's do that. Zaheva will catch up.
-Tact: ... We don't have camping supplies, do we?
-Ajax: Nope! I have a couple of torches but we shouldn't light them unless we need them. Looks like we'll be bundling up in our cloaks for a quick rest.
-Tact: Not quite what I'm used to.
-Xena: This is what being on the run is like. Never thought I'd end up in this position, especially not this late in life...
-Ajax: As this war went on, I knew it was only a matter of time 'til I found myself out here on the run. War breeds the worst in people, and our leaders are no exceptions.
-Tact: I've never thought about it that way... We always see the Grand Magi as such infallible leaders...
-Ajax: Well, now you've seen firsthand how wrong that can be.
-Zaheva: Finally, I caught up to you!
-Ajax: What's the problem, Zaheva?
-Zaheva: We're being followed.
+[ConversationText]
+[FarLeft][LoadPlayer][Left][LoadXena]
+It's starting to get dark out. Ajax, should we set up camp?
+[MidRight][LoadAjax]I see somewhere off road up here that should leave us relatively out of sight. Let's do that. Zaheva will catch up.
+[FarLeft]... We don't have camping supplies, do we?
+[MidRight]Nope! I have a couple of torches but we shouldn't light them unless we need them. Looks like we'll be bundling up in our cloaks for a quick rest.
+[FarLeft]Not quite what I'm used to.
+[Right]This is what being on the run is like. Never thought I'd end up in this position, especially not this late in life...
+[MidLeft]As this war went on, I knew it was only a matter of time 'til I found myself out here on the run. War breeds the worst in people, and our leaders are no exceptions.
+[FarRight]I've never thought about it that way... We always see the Grand Magi as such infallible leaders...
+[MidLeft]Well, now you've seen firsthand how wrong that can be.
+[FarFarLeft]Finally, I caught up to you![CloseSpeech][FarRight][ClearFace][Right][MoveFarRight][MidLeft][MoveRight][ClearFace][LoadAjax]
+What's the problem, Zaheva?
+[MidLeft][LoadZaheva]We're being followed.
 [Events]
-Ajax: Arcanians? I didn't think they'd be able to mobilize so quickly.
-Zaheva: No, Ajax... Not Arcanians.
-Xena: Mistlain wouldn't... Did he hire Shizonians?!
-Zaheva: Yes.
-Tact: Shizonians? The assassins from the isle south of Avenir?
-Xena: The very same. This won't be easy... Ajax, you said you had torches, right?
-Ajax: Yep. I'll set them up around the perimeter. It'll give us advance notice of their approach, but if they destroy them, there's nothing we can do.
-Xena: Save a few for us too.
-Ajax: Right!
-[X]
+[Right]Arcanians? I didn't think they'd be able to mobilize so quickly.
+[MidLeft]No, Ajax... Not Arcanians.
+[FarRight]Mistlain wouldn't... Did he hire Shizonians?!
+[MidLeft]Yes.[CloseSpeech][MoveLeft][FarLeft][LoadPlayer]
+Shizonians? The assassins from the isle south of Avenir?
+[FarRight]The very same. This won't be easy... Ajax, you said you had torches, right?
+[Right]Yep. I'll set them up around the perimeter. It'll give us advance notice of their approach, but if they destroy them, there's nothing we can do.
+[FarRight]Save a few for us too.
+[Right]Right![X]
 
 ## Mage3LeaveBossHereText
-Pascal: These fools... Trying to escape the wrath of a Grand Magus? I wouldn't want to be them.
-Arya: Pascal...
-Pascal: I know this is your first high-profile job, Arya. But this is what we Shizonians do.
-Arya: I understand. I won't let our people down.
-Pascal: They're setting up torches. We'll need to break those to reduce their sight, that way, we can use the night to our advantage.
-[X]
+[ConversationText]
+[MidLeft][LoadArya][MidRight][LoadPascal]
+These fools... Trying to escape the wrath of a Grand Magus? I wouldn't want to be them.
+[MidLeft]Pascal...
+[MidRight]I know this is your first high-profile job, Arya. But this is what we Shizonians do.
+[MidLeft]I understand. I won't let our people down.
+[MidRight]They're setting up torches. We'll need to break those to reduce their sight, that way, we can use the night to our advantage.[X]
 
 ## Mage3LeaveTorchesUpText
-Zaheva: They're starting to move in. Get ready, everybody! If we can keep the torches up, we'll have a better chance against them!
-[X]
+[ConversationText]
+[Left][LoadZaheva]They're starting to move in. Get ready, everybody! If we can keep the torches up, we'll have a better chance against them![X]
 
 ## Mage3LeaveRecruitAryaText
-Arya: You're... One of the Arcanians... I have to kill you!
-Tact: Wait! I can see your hesitancy.
-Arya: What!? Don't try to distract me!
-Tact: Do you honestly believe in what you're doing? Killing a few innocent people who were scapegoated by a ruler they trusted!?
-Arya: It's not my job to care why it is you have to die! I have a job to do...
-Tact: What's your name?
-Arya: Why should I tell you?
-Tact: You don't have to do this. This war... This conflict... None of it matters, does it? Yet here you are, blindly propagating it.
-Arya: ... Arya. My name is Arya.
-Tact: Mine's [Tact]. You should come with us.
-Arya: But...
-Tact: Make your choice. But you're not gonna kill me, are you?
-Arya: ...
-Tact: Then your choice is made.
-Arya: Dammit all... Fine, fine! I'll help you out.
-[X]
+[ConversationText]
+[MidRight][LoadPlayer][MidLeft][LoadArya]
+You're... One of the Arcanians... I have to kill you!
+[MidRight][MoveRight]Wait! I can see your hesitancy.
+[MidLeft][MidLeft]What!? Don't try to distract me!
+[MidRight]Do you honestly believe in what you're doing? Killing a few innocent people who were scapegoated by a ruler they trusted!?
+[MidLeft]It's not my job to care why it is you have to die! I have a job to do...
+[MidRight]What's your name?
+[MidLeft]Why should I tell you?
+[MidRight]You don't have to do this. This war... This conflict... None of it matters, does it? Yet here you are, blindly propagating it.
+[MidLeft]... Arya. My name is Arya.
+[MidRight]Mine's [Tact]. You should come with us.
+[MidLeft]But...
+[MidRight]Make your choice. But you're not gonna kill me, are you?
+[MidLeft]...
+[MidRight]Then your choice is made.
+[MidLeft]Dammit all... Fine, fine! I'll help you out.[X]
 
 ## Mage3LeaveAryaPascalBattleText
-Pascal: Arya! You betray us?
-Arya: I'm sorry Pascal... But I just can't do this!
-Pascal: You have forsaken the laws of our land. I will take it upon myself to be your judge. Come!
-[X]
+[BattleText]
+[FarLeft][LoadPascal]
+Arya! You betray us?[ClearFace]
+[FarRight][LoadArya]
+I'm sorry, Pascal... But I just can't do this![ClearFace]
+[FarLeft][LoadPascal]
+You have forsaken the laws of our land. I will take it upon myself to be your judge. Come![X]
 
 ## Mage3LeaveZahevaPascalBattleText
-Pascal: You... Your name is Zaheva.
-Zaheva: Familiar with me, are you?
-Pascal: Your contract was terminated when my companions failed to return after hunting you. Perhaps today I shall collect what they failed to!
-[X]
+[BattleText]
+[FarLeft][LoadPascal]
+You... Your name is Zaheva.[ClearFace]
+[FarRight][LoadZaheva]
+Familiar with me, are you?[ClearFace]
+[FarLeft][LoadPascal]
+Your contract was terminated when my companions failed to return after hunting you. Perhaps today I shall collect what they failed to![X]
 
 ## Mage3LeaveXenaPascalBattleText
-Pascal: You're Xena, are you?
-Xena: I presume I am your primary target here?
-Pascal: You assume correctly.
-Xena: Unfortunately for you, I won't be going down without a fight.
-Pascal: I'd have it no other way!
-[X]
+[BattleText]
+[FarLeft][LoadPascal]
+You're Xena, are you?[ClearFace]
+[FarRight][LoadXena]I presume I am your primary target here?[ClearFace]
+[FarLeft][LoadPascal]You assume correctly.[ClearFace]
+[FarRight][LoadXena]Unfortunately for you, I won't be going down without a fight.[ClearFace]
+[FarLeft][LoadPascal]I'd have it no other way![X]
 
 ## Mage3LeavePascalDeathText
-Xexim... I'm sorry... We failed you...
-[X]
+[BattleText]
+[FarLeft][LoadPascal]
+Xexim... I'm sorry...[CloseEyes] We failed you...[X]
 
 ## Mage3LeaveEndingText
-Ajax: We managed to fend them off...
-Zaheva: Most of them.
-Tact: Wait, Zaheva!
-Zaheva: She's one of them!
-Arya: She's right, [Tact]... I came here with the intention to kill all of you.
-Tact: But you realized the error in that!
-Arya: The fact remains, I broke the laws of Shizon. One way or another, my fate is sealed.
-Zaheva: In other words, I have her permission to lop her head off.
-Ajax: Zaheva, stop. We don't have time for this. Mistlain surely has Arcanians en route to confirm the deed is done. We don't have the energy to deal with that. Besides, adding Arya to our little group will only be helpful in the long run.
-Zaheva: And when she tries to kill us?
-Tact: She had every opportunity to kill me. Besides, why fake betraying the Shizonians only to get back at us later? It puts her at a disadvantage!
-Xena: [Tact] is right. Let's trust Arya. She'll need people accompanying her now anyway.
-Arya: [Tact]... Thank you.
-Ajax: Now that that's figured out, we should get a brief bit of rest then try to get to Bacrun as fast as possible. I'll take watch. We'll set out in a few hours, before first light.
-[X]
+[ConversationText]
+[Right][LoadZaheva][FarRight][LoadAjax]
+We managed to fend them off...
+[Right]Most of them.[MidLeft][LoadArya][FarRight][ClearFace][LoadPlayer]
+Wait, Zaheva!
+[Right]She's one of them!
+[MidLeft]She's right, [Tact]... I came here with the intention to kill all of you.
+[FarRight]But you realized the error in that!
+[MidLeft]The fact remains, I broke the laws of Shizon. One way or another, my fate is sealed.
+[Right]In other words, I have her permission to lop her head off.[CloseSpeech][MidLeft][MoveLeft][FarLeft][LoadAjax]
+Zaheva, stop. We don't have time for this. Mistlain surely has Arcanians en route to confirm the deed is done. We don't have the energy to deal with that. Besides, adding Arya to our little group will only be helpful in the long run.
+[Right]And when she tries to kill us?
+[FarRight]She had every opportunity to kill me. Besides, why fake betraying the Shizonians only to get back at us later? It puts her at a disadvantage![Right][ClearFace][LoadXena]
+[Tact] is right. Let's trust Arya. She'll need people accompanying her now anyway.
+[MidLeft][Tact]... Thank you.
+[FarLeft]Now that that's figured out, we should get a brief bit of rest then try to get to Bacrun as fast as possible. I'll take watch. We'll set out in a few hours, before first light.[X]
