2d6f07c6bad90f86c77213757cc9a65b25b516be
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2022-04-13 22:36:25 -0400

    Development on mage 4 leave

---
+++ Text/Chapters/Mage/Mage4Leave.s
@@ -0,0 +1,92 @@
+“Dry Trail”
+Zaheva: [Tact], wake up.
+Avatar: What is it?
+Zaheva: We need to move out.
+Ajax: It's still a few days ride to Bacrun and, unfortunately, most of that is through a warzone. We'll have to move quickly and quietly, which means moving in the early and late hours and trying to lay low during the day.
+Avatar: What if we run into part of Arcanae's forces?
+Arya: Moving without being noticed is my specialty. Just follow my lead.
+Ajax: You can take the lead, Arya. I trust you know a good way to Bacrun from here too?
+Arya: Yeah, and we can easily steer clear of the major roadways on our way there too.
+Ajax: Good. Let's go.
+
+
+Zaheva: Why do we trust the assassin?
+Ajax: We all have dark secrets, Zaheva. By all rights, none of us have reason to trust each other.
+Zaheva: But we all escaped Arcanae together. She came with the intent to kill us.
+Ajax: I know you're traumatized by the things of your past. But you have to count on all of us.
+Zaheva: ... Fine.
+Xena: To Ajax's point, you and he sort of just appeared. I still don't know either of your motivations.
+Zaheva: My motivations are my own.
+Ajax: I can't stand to watch Arcanae become the corrupt entity it has. If we can make a difference in that, I'm willing to help.
+Zaheva: Considering all we are now is on the run, I don't know if that's really worked out for you.
+Xena: Getting to safety is the first priority. Then we can decide on a course of action from there. We're all exiles from our homelands at this point. That should be reason enough for us to trust each other.
+Zaheva: Hmph.
+Arya: Not to interrupt, but I scouted a bit ahead and... we may have a problem.
+
+*cut to battlefield where the Cesarian and Arcanian armies are engaged*
+“The Devoted”
+Avatar: That's-
+Xena: A battlefield. It looks like a platoon of Cesarian soldiers engaged with Arcanian soldiers. I don't recognize either commander, so it's just a small skirmish.
+Zaheva: The Arcanian in charge is one Sergeant Gerald. I served under him for a brief excursion into Cesaria awhile ago. Not anyone we should be worried about.
+Xena: Either way, I don't fancy getting caught by any Arcanians right now.
+Ajax: It could be to our benefit to try and connect with the Cesarian commander.
+Avatar: Why's that?
+Xena: Ajax is right. Cesaria could provide us with some safety in exchange for any information we would be able to provide.
+Arya: What about escaping? I thought the goal was to get away from this and figure out a plan from there.
+Xena: Both are valid options, but the best escape route is through the center of the battlefield. Going around would take too long and risk us getting noticed by their rear guards. If we go through the battlefield, we'll be unnoticed amidst the chaos.
+Zaheva: So escape, or meet up with the Cesarian commander. Makes no difference to me.
+Xena: If we want to escape, we need to push to the north as fast as possible. [Tact], you take the lead. We'll follow suit.
+Avatar: Right. We'll have to move quickly... Let's go!
+
+Player Phase: Chinese Tea
+Enemy Phase: Puppet Ganon
+(for now)
+
+*talk to Blaine w/ Tact*
+Blaine: Who are you? What are you doing here on a battlefield? State your business, quickly, before I cut you down, Arcanian dog.
+Avatar: I am [Tact], a former student of Arcanae. My friends and I are outcasts, betrayed by the Grand Magus Mistlain...
+Blaine: Betrayed? Hm. I will give you a chance, but we do not have time to discuss this any further. Assist me in killing the Arcanian commander of this battlefield, and perhaps I will hear you out... Remus! Adeline! Assist [Tact] here. You all are going to lead a counterassault against the Arcanian commander!
+Remus: Sir, yes, sir!
+Adeline: Understood!
+
+*against boss*
+w/ Zaheva
+Zaheva: It's been awhile, Gerald.
+Gerald: Zaheva! I thought you'd been killed... Can't say I'm surprised to see you betray Arcanae. Perhaps this is my chance to burn you to a crisp as I should have ages ago!
+Zaheva: Only if you can avoid finding yourself impaled by my sword...
+
+Generic: You all aren't Cesarians... Why have you entered this battlefield!? Grr... I'll deal with you myself!
+Death: I've... Failed...
+
+*escape dialogue* (chapter ends when any individual escapes)
+Arya: Let's go, [Tact]! No time to dally!
+Zaheva: I'm moving ahead. Catch up quickly.
+Ajax: We made it to the other side, let's go!
+Xena: Don't fall behind, [Tact], we have to keep moving!
+Avatar: We made it through the skirmish... On to Bacrun!
+
+*if chapter won by defeating boss*
+“Heaven's Rock Seat”
+(if Blaine wasn't spoken to earlier) Blaine: You! Who are you? Why did you enter this battlefield? State your business before I cut you down, Arcanian dog!
+(if Blaine was spoken to earlier) Blaine: Hmph. You don't fight too badly. Thank you for the assistance. Now, tell me why you're here. My earlier threat of cutting you down still stands.
+Xena: We're former Arcanian researchers under Grand Magus Mistlain. Something went wrong on a mission, and he betrayed us. Rather than wait to be executed, we fled.
+Blaine: Hm... Why was it that you were betrayed?
+Xena: We had come into conflict with some soldiers under Xavus and Uzkalis. Rather than stand up to the other Magi, Mistlain decided to heed their demands and threaten to execute [Tact] and I. Zaheva and Ajax came to our rescue, and we were fleeing to Bacrun to try and regroup before deciding on a course of action.
+Blaine: So... you wish to see the governmental structure of Arcanae torn down?
+Xena: Arcanae used to stand for research. Progression. Science. What they are now... It is not where I grew up. People like Ajax and [Tact] deserve to see what Arcanae was meant to be, and under the current situation, it will never look like that.
+Blaine: I have no love for your kind. However... We also wish to see Arcanae dismantled. Xavus and Uzkalis have gone too far, and if they're truly strong-arming the other Magi that way... Then we have no choice. I am Vice-Captain Blaine of the Lion Brigade under Captain Jurgen.
+Xena: Thank you. My name is Xena.
+Blaine: Unfortunately, I do not have the influence to immediately offer you assistance, whether that be safety or an alliance. I will have to commune with my Captain, and his attitude toward Arcanians is no friendlier than mine. Until then, I will have to treat you as prisoners.
+Zaheva: Great going, [Tact]...
+Avatar: It'll be fine. We'll be safer than if we just keep running.
+Xena: Thank you, Vice-Captain, even if we cannot come to an agreement, simply listening to our plight means a lot.
+
+*if escaped*
+“Victory Minor”
+Arya: We made it...
+Avatar: That battle was intense. There was no hesitancy on either side...
+Ajax: Cesaria and Arcanae have no love for each other. Years of divide have created the hatred we saw.
+Zaheva: Clearly, Arcanians have little love for each other either.
+Avatar: I suppose I have begun to see that...
+Xena: It's okay. We made it through that. Hopefully that battle will slow down any Arcanians that remain pursuing us.
+Ajax: We're getting very near to Bacrun. Hopefully once we get there, we can find some way to escape from Arcanae even further, either to Haedric, Altia, or the far northern parts of Cesaria. For now, I think we should take a brief break. Get some rest, we'll move out again in a few hours.
