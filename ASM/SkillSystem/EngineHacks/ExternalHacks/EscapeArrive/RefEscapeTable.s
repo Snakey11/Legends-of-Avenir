0b2850efbb9ccec08f16fec51c2ad6767b88d1c3
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2022-04-16 17:07:23 -0400

    Reworking how escape works internally

--- ASM/SkillSystem/EngineHacks/ExternalHacks/EscapeArrive/RefEscapeTable.s
+++ ASM/SkillSystem/EngineHacks/ExternalHacks/EscapeArrive/RefEscapeTable.s
@@ -1,24 +1,69 @@
+
 .thumb
-.align 4
 
-push {r14}
-ldr r0,SVAL
-ldr r2,SVAL
-ldrb r0,[r0]
-@r0 = char ID
-mov r1,#2
-mul r0,r1
-ldr r1,TextTable
-add r0,r1
-ldrh r0,[r0]
-strh r0,[r2]
-pop {r1}
-bx r1
+.macro blh to, reg
+    ldr \reg, =\to
+    mov lr, \reg
+    .short 0xF800
+.endm
 
+/*
+extern struct {
+  u8 charID;
+  u8 chapterID;
+  u16 textID;
+} EscapeQuoteList;
+*/
+
+.equ gEventSlot, 0x30004B8
+.equ gChapterData, 0x202BCF0
+.equ GetUnitStructFromEventParameter, 0x800BC50
+
+@ Memory slot 0x2 has the active unit character ID.
+@ Return the text ID to show in memory slot 0xC.
+.global RefEscapeTable
+.type RefEscapeTable, %function
+RefEscapeTable:
+push { r4 - r6, lr }
+ldr r0, =gEventSlot
+ldr r4, [ r0, #0x4*0x2 ] @ Current character ID.
+ldr r0, =gChapterData
+ldrb r5, [ r0, #0x0E ] @ Current chapter.
+ldr r6, =EscapeQuoteList
+StartRefLoop:
+	@ Check for character ID matching.
+	ldrb r0, [ r6 ] @ Character ID for this entry.
+	cmp r0, #0x00
+	beq RefEscapeTableNotFound @ Terminator
+	
+	blh GetUnitStructFromEventParameter, r1
+	cmp r0, #0x00
+	beq ReitRefLoop @ This character ID doesn't even exist. Exit.
+	ldr r0, [ r0 ]
+	ldrb r0, [ r0, #0x04 ] @ Character ID.
+	cmp r0, r4
+	beq ReitRefLoop
+	
+	@ Check for chapter ID matching.
+	ldrb r0, [ r6, #0x01 ] @ Chapter for this entry.
+	cmp r0, #0x00
+	beq FoundEntry @ No chapter condition.
+		cmp r0, r5
+		bne ReitRefLoop
+	FoundEntry:
+		ldrb r0, [ r6, #0x02 ]
+		b StoreToSlot
+	
+	ReitRefLoop:
+	add r6, r6, #0x04
+	b StartRefLoop
+
+RefEscapeTableNotFound:
+mov r0, #0x00
+StoreToSlot:
+ldr r1, =gEventSlot
+str r0, [ r1, #0x04*0x0C ] @ Memory slot 0xC.
+pop { r4 - r6 }
+pop { r0 }
+bx r0
 .ltorg
-.align 4
-
-SVAL:
-.word 0x30004C0
-TextTable:
-@POIN EscapeTextTable
