988c4b6e1b8259942d33c22dd8cb7c3b8328d537
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2022-05-19 02:56:22 -0400

    Mage 5 Leave Kill formatted text

---
+++ Text/Chapters/Mage/Mage5LeaveKill.s
@@ -0,0 +1,115 @@
+
+## Mage5LeaveKillStartingText
+[ConversationText]
+[Right][LoadXena][FarRight][LoadPlayer][MidLeft][LoadJurgen]
+My Vice-Captain has brought me up to speed. So, most of you are Arcanian deserters, with the exception of the assassin...
+[Right]That's correct.
+[MidLeft]Hmph. I'm not one to up and trust Arcanians, but Blaine says that you aided them well in their skirmish. I'll give you a chance to prove your loyalty. After that, we can talk.
+[FarRight][CloseEyes]You're gonna send us out to fight our own people when we just tried to escape?
+[MidLeft]You all seem capable. You can handle it.
+[FarRight][OpenEyes][MoveFarRight]That's-!
+[Right][Tact]! We can't bargain right now. The Captain has no reason to trust us. We'll have to do as he says.
+[MidLeft][Smile]Yes, [Tact], listen to your teacher.
+[Right]What is it you want?
+[MidLeft][Smile]There is a small fort on the far side of the border that Arcanians have been using as a hub to make brief excursions into our territory. Go there and eliminate the inhabitants.
+[Right]Very well. Thank you for your time, Captain.
+[MidLeft]Oh, one last thing... I'll be sending Corporal Adeline and Private Remus with you to observe. They are free to help you in battle, but I want to make sure someone is there observing.[X]
+
+## Mage5LeaveKillAngryText
+[ConversationText]
+[MidLeft][LoadXena][MidRight][LoadPlayer]
+You agreed to that?
+[MidLeft]What choice do we have? We can't make an enemy of both Cesaria and Arcanae.[CloseSpeech][MoveLeft][MidRight][MoveRight][FarRight][LoadArya][FarLeft][LoadZaheva]
+Speak for yourself.
+[FarRight]I'd have made an enemy of Shizon too. I'd rather not have three major powers trying to kill me...
+[Left]We don't have any other choice. If any of you want to leave now, you can. But I think that if we go through with this and can make an ally of Cesaria, we can help them strike back against Arcanae in immense ways. We know the lay of the land, we know the capital, we know the leaders. This could be our way to get our revenge.
+[FarRight]I agree.
+[Right]I suppose you're right that we don't have much choice...
+[FarLeft]... Fine.[FarRight][ClearFace][LoadAdeline][ClearFace]
+You're the Arcanians?
+[Left]Corporal Adeline, I presume?
+[FarRight]Yes. I've been assigned to travel with you on this mission along with Corporal Remus. He's getting a few supplies ready right now, I thought I would swing by and make sure you're ready to leave.
+[FarLeft]We're ready, Cesarian.
+[FarRight]... Very well. We will head out shortly.[ClearFace]
+[......]
+[FarLeft]You can sense her disgust towards us.
+[Left]You can't blame her. Or any of them, really.
+[FarLeft]Whatever. Let's get going.[X]
+
+## Mage5LeaveKillTravelText
+[ConversationText]
+[MidRight][LoadAdeline]
+That's the fort that Captain Jurgen wants us to take.[FarLeft][LoadXena][Left][LoadPlayer]
+It doesn't look too well-defended...
+[MidRight]No, it shouldn't be. We've been weakening Arcanae's presence in this region the past few weeks, so we should be in a very good place.
+[FarLeft]Let's just deal with this. They won't be expecting us, so we should be able to take care of this cleanly and easily.
+[MidRight]Is it like Arcanians to just charge into a battle with no strategy? It could be smarter to split up. Most of us act as a diversion, with a few select individuals sneaking into the fort from the back to strike at their back.
+[FarLeft]Either could work well. What do you think [Tact]?[X]
+
+## Mage5LeaveKillDirectText
+[ConversationText]
+[MidRight][LoadAdeline][FarLeft][LoadXena][Left][LoadPlayer]
+Let's just do a direct assault. We've established their defenses are relatively weak, so it shouldn't prove an issue.
+[MidRight]Hmph. For a ton of fools who do nothing but research, you all don't care for strategy.
+[FarLeft]We may be working together, Corporal, but I will not have you criticizing my student.
+[MidRight]Very well. I am simply here as an observer, after all.
+[Left]Shall we mount our assault?
+[FarLeft]Yes, let's move in.[X]
+
+## Mage5LeaveKillDiversionText
+[ConversationText]
+[MidRight][LoadAdeline][FarLeft][LoadXena][Left][LoadPlayer]
+I think Adeline is right, creating a diversion could be smart.
+[FarLeft]Who would go with the smaller strike force?[CloseSpeech][MidRight][MoveRight][FarRight][LoadArya]
+I'm good at this sort of thing. I can go.[FarLeft][ClearFace][LoadZaheva]
+I'm not letting you out of my sight, Shizonian. I'm going too.
+[Right]I'm not about to let any of you execute part of this operation without observation. Remus![FarLeft][ClearFace][LoadRemus]
+Yes?
+[Right]You're going with Arya and Zaheva. Follow their lead, but keep an eye on them.
+[FarLeft]Understood.[ClearFace]
+[FarRight]We'll find a back way into the fort and strike from behind while your frontal assault distracts them. Good luck.
+[Left]Thanks, Arya. The rest of us will lead a frontal assault. Hopefully we'll catch them off-guard and we'll be able to finish this quickly. Let's do this.[X]
+
+## Mage5LeaveKillSeanBattleText
+[BattleText]
+[FarLeft][LoadSean]
+Some of you are Arcanians... Traitors![X]
+
+## Mage5LeaveKillSeanDeathText
+[BattleText]
+[FarLeft][LoadSean]
+You can't run from Arcanae... Forever...[X]
+
+## Mage5LeaveKillEndingText
+[ConversationText]
+[MidRight][LoadXena][MidLeft][LoadAdeline]
+[MidLeft]We're lucky that that worked out as planned.
+[MidRight]Perhaps you should have had more faith in us.
+[MidLeft]Perhaps. I daresay the Vice-Captain will be tremendously impressed with your work here, Arcanian.
+[MidRight]The credit goes to all of us, not just me.
+[MidLeft]Hmph.
+[FarFarLeft]Adeline![CloseSpeech][MidRight][MoveFarRight][MidLeft][MoveRight][ClearFace][LoadAdeline]
+What is it, Remus?
+[MidLeft][LoadRemus]A soldier from the Cheetah Brigade is here. She wishes to speak with us. All of us.
+[Right]The Cheetah Brigade? Why would they... Ugh. Let's go.[X]
+
+## Mage5LeaveKillYvetteText
+[ConversationText]
+[MidLeft][LoadAdeline][MidRight][LoadYvette]
+Ah, Corporal Adeline. Private Remus. The Arcanians. I'm Sergeant Yvette, a soldier from the Cheetah Brigade.
+[MidLeft]What are you doing here?
+[MidRight]Captain Zalius has taken notice of the Arcanian defectors. He believes they could be indispensable in his current work.
+[MidLeft]They're with the Lion Brigade. Tell your Captain-
+[MidRight]That won't be necessary. Here's an official missive bearing both Captain Zalius and Captain Jurgen's seals. They have been reassigned to work under the Cheetah Brigade.
+[MidLeft]The Arcanian defectors Xena, [Tact], Ajax, Zaheva... Shizonian Arya... [MoveMidLeft]Corporal Adeline and Private Remus!?
+[MidRight]Ah, yes. Captain Jurgen specifically requested you two be brought along as well. It seems he... distrusts those of us in the Cheetah Brigade.[ClearFace][FarRight][LoadXena][Right][LoadPlayer]
+Teacher, what's the Cheetah Brigade?
+[FarRight]I'm not entirely sure...[ClearFace][LoadZaheva]
+They're basically the shadow of the Cesarian military. Work as assassins, spies, information gatherers... You name it.
+[MidLeft]Arcanian, how do you know-[FarRight][ClearFace][LoadArya]
+Shizon's leader, Xexim, always spoke highly of their Captain Zalius... He seemed to be one of the few Cesarians Xexim respected.[Right][ClearFace][LoadXena]
+So they likely want us for what we know.
+[MidLeft]Well, Sergeant,[Right][ClearFace][FarRight][ClearFace][MidRight][LoadYvette][MidLeft] we are under your command for the time being.
+[MidRight]Perfect. Captain Zalius has a hideout near here where he will brief us on what our mission will be. For now, get rest. You all did well here at the fort, I'll fly back to your Captain Jurgen to deliver a report that the mission was successful.
+[CloseSpeech][MidLeft][MoveLeft][FarLeft][LoadRemus]Thank you, Sergeant. We appreciate it.
+[Left]Yes, as Private Remus said. Thank you, Sergeant. We will set out to meet with your Captain as soon as we have rested.[X]
