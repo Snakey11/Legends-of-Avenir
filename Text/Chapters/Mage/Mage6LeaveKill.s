27ab37743556017acb572e7c115f54f372792889
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2022-07-14 12:36:39 -0400

    Progress on new mage chapter 6

---
+++ Text/Chapters/Mage/Mage6LeaveKill.s
@@ -0,0 +1,123 @@
+
+## Mage6LeaveKillStartingText
+Zalius: Ah, the Arcanians. You've arrived. I am pleased you've chosen to meet with me.
+Xena: I am Xena, formerly subservient to Mistlain. Your Sergeant told us that you may have need of us?
+Zalius: Yes, I do. You see, I've recently received some intelligence that Xavus is preparing some sort of super weapon.
+Avatar: A super weapon? What kind of super weapon?
+Zaheva: Xavus and Uzkalis are just seeking some kind of convenient way to end the war.
+Zalius: Perhaps. The nature of the weapon is unclear. Despite this, it's not something I can bring myself to ignore. Such a weapon could spell the end of Cesaria, or worse, Avenir as a whole.
+Xena: I never served under Xavus, so I know nothing about this weapon.
+Zalius: I assumed as much. However, are you familiar with the Library of the Magi?
+Xena: I've heard rumors of its existence. A hidden underground library of texts, artifacts, and weapons only accessible to the Grand Magi. Given the conflicts between the Grand Magi for the past few decades, however, I always assumed it didn't exist.
+Zalius: I believe that that location is where much of the research and development on this weapon is taking place. Are you sure you don't know where it is?
+Xena: I'm still not convinced it exists.
+Zalius: Hrm. Still, you know the layout of Arcanae better than anyone here. Is there anywhere you can think it might exist?
+Xena: If it's underground, it could be just about anywhere...
+Zaheva: Could always just ask a Grand Magus.
+Xena: What?
+Zaheva: We can drop back in on Mistlain, knock him around a bit, demand he tell us where it is, then make a stop at the library and break Xavus' toy. Done.
+Zalius: I like this one's thinking.
+Avatar: Are you insane, Zaheva!? We barely got out of there with our lives!
+Zaheva: There's more of us now, and we'd have the element of surprise.
+Zalius: I agree with [Tact], actually. While the idea is good, a direct assault on Mistlain would be foolhardy.
+Xena: Mistlain often takes brief trips out of the capital, only guarded by a few of his elite guards. It wouldn't be easy, but it would be more manageable than his entire fighting force.
+Zalius: Yes... I've heard word of these trips. I can have some men keep an eye out for his departure, and while he's gone, you can strike at him.
+Arya: Would I be able to assist in the scouting effort?
+Zalius: You're the Shizonian, yes?
+Arya: That's right.
+Zalius: I'd love for you to assist. My scouts could learn something from you.
+Avatar: Are you sure about this, Arya?
+Arya: I want to make sure I see all of it with my own eyes. That way, if it's something I don't think we can take, I can warn you as such.
+Avatar: I understand. Thank you.
+Zalius: Until they return with further news, take a rest.
+Xena: Thank you, Captain.
+[X]
+
+## Mage6LeaveKillRestText
+Xena: [Tact], are you nervous to face Mistlain?
+Avatar: I mean, it does seem ironic to me that we spent all that time on the run from him only to now be turning around to try and get information from him.
+Xena: I was having the exact same thought, to be honest...
+Zaheva: We only had to run because he had the jump on you. This time, we have a distinct advantage.
+Xena: I would tend to agree with you. They won't know we're coming, we've had a lot of experience since then, and we have new allies...
+Zaheva: We're also not on his turf.
+Xena: That's also right.
+Avatar: But even if we win this, can we go up against Xavus and Uzkalis' elite?
+Xena: We'll just have to see.
+[Events]
+Arya: I'm back. 
+Ajax: How is it looking?
+Arya: Mistlain is only travelling with a few of his guards. Given that most of them are researchers and not fighters, we should be well within our ability to take them out. The big question is if we'll be able to get the information we need out of him.
+Zalius: Leave that to us.
+Arya: Oh?
+Zalius: Being information gatherers doesn't just mean the Cheetah Brigade have keen eyes and ears. We have other methods as well.
+Xena: I'll... take your word for it.
+Avatar: I'm glad we're on your good side...
+Zalius: Alright, all of you will move in on them. I have some other business to attend to, but Yvette will be here to assist. She will take charge of getting the information from Mistlain. As soon as we know where the library is, send a missive to me immediately. You'll need all the help you can get to breach their defenses. Good luck out there.
+[X]
+
+## Mage6LeaveKillMistlainText
+Arcanian: Milord, we're making good time.
+Mistlain: Good. With the war as it is now, I'd prefer to spend as little time on the road as possible...
+Arcanian: Afraid of your colleagues?
+Mistlain: So long as they never find out about the... incident, we're safe... Regardless, I don't trust any of them nor do I trust the Cesarians...
+Arcanian: The escaped scholars? I understand. Don't worry, milord, we are here to keep you safe.
+Other Arcanian: Lord Mistlain! There is movement off the road!
+Mistlain: Form up! Defend me while I prepare my escape!
+[X]
+
+## Mage6LeaveKillStartBattleText
+Remus: I guess they've spotted us.
+Xena: Move in now, while they're scrambling! Make an opening for Yvette to get to Mistlain!
+[X]
+
+## Mage6LeaveKillMistlainBattleText
+I don't know what it is you want with me, but I won't yield!
+[X]
+
+## Mage6LeaveKillTactMistlainBattleText
+Xena's apprentice? So this is revenge, huh? You'll learn I'm more powerful than I seem.
+[X]
+
+## Mage6LeaveKillXenaMistlainBattleText
+Mistlain: You know it wasn't anything personal, Xena, right?
+Xena: I don't care. You ruined me.
+Mistlain: Very well, then! I will have you just end your existence!
+[X]
+
+## Mage6LeaveKillMistlainDeathText
+[BattleText]
+[FarLeft][LoadMistlain]
+I should have known...[CloseEyes] siding with New Arcanae was a mistake...[X]
+
+## Mage6LeaveKillMistlainCapturedText
+Yvette: We have you, Mistlain. Surrender.
+Mistlain: Who are you?
+Yvette: My name is Yvette of the Cesarian Cheetah Brigade. Answer my questions, and this will go far more smoothly.
+Mistlain: Cesarians!? Then... this must be about the war. What do you want?
+Yvette: The secret library of Arcanae. Where is it?
+Mistlain: That place? I can't- Ah!
+Yvette: Yes you can. Uzkalis and Xavus have a plan brewing and we're fairly confident that it's going to happen there. Cesaria isn't the only place with things to lose. You really think they'll keep you around when they seize power?
+Mistlain: Regardless, it is a long standing tradition of the Magi to never disclose- Agh!
+Yvette: Talk, or this blade will make you talk.
+Mistlain: It's in... the southern regions of Arcanae... There's an old ruin, and the entrance can be reached by using one of our Sigils...
+Yvette: Sigil?
+Mistlain: All five of the Grand Magi have one. They're the symbol of our role in Arcanian society. Here, use mine! Just please... Let me go...
+Yvette: Very well. Thank you, Lord Mistlain. You may go.
+*Mistlain leaves*
+Avatar: That was a little scary to witness, Yvette...
+Yvette: I apologize, [Tact]. Thankfully we didn't have to harm him more than necessary.
+Xena: The man is a coward. I'm not surprised he caved to you so easily.
+Yvette: Right. Let's get back to headquarters and tell the Captain what we found.
+
+## Mage6LeaveKillMistlainDeadText
+Yvette: Dammit, we needed him alive!
+Avatar: I'm sorry...
+Yvette: We're going to lose a lot of time trying to figure out the location of that thing. Is there anything on his body that might help?
+Zaheva: I'll check... Hm... Ah, the old man has his Sigil on him.
+Yvette: Sigil?
+Zaheva: Basically a crest. Each Magi has one. As far as I know, they're purely ceremonial, but having it could help us out.
+Ajax: Not to mention we'll need it to help stabilize Arcanae once this is all said and done, now that they'll need to find a new Grand Fire Magus...
+Avatar: Not to mention probably new Grand Thunder and Dark Magi as well.
+Yvette: For now, we need to get back to Zalius and see what other options we have. Maybe we can reach Lady Muriel easily and she can give us the information we need, but she's had her own hands tied for a while...
+Avatar: Let's go. There's nothing else for us here.
+[X]
